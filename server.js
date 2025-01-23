eb09d23409cb942e4a9818cf3e5d62831977bd4d
Aaron Taylor <aarontayloraz@gmail.com>
2025-01-22 20:27:29 -0700

    Added .babelrc, created server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(path.join(__dirname, './client/dist')));
+const PORT = process.env.PORT;
+app.listen(PORT);
+console.log(`Server listening at http://localhost:${PORT}`);
