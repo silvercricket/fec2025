42860a03acabf4e22f0f5c2908323a0f7f2d7e56
albertolovell <alberto.lovell@gmail.com>
2025-01-23 23:04:01 -0800

    similar and outfit carousel skeleton

--- client/src/components/Similar/similar.jsx
+++ client/src/components/Similar/similar.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Slider from 'react-slick';
+import '../../../assets/styles.css';
 
 
 
@@ -24,10 +25,19 @@ const Similar = ({product, setProduct}) => {
     }, []);
 
     var settings = {
-      dots: true,
+      arrows: true,
       infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnHover: true,
       speed: 500,
       slidesToShow: 3,
+      slidesToScroll: 1
+    };
+    var settings2 = {
+      speed: 500,
+      infinite: false,
+      slidesToShow: 1,
       slidesToScroll: 1
     };
 
@@ -36,18 +46,26 @@ const Similar = ({product, setProduct}) => {
       <div>
         Similar products go here!
       </div>
-      <ul>
-        {products.map((product) => (
-          <>
-          <Slider {...settings}>
-          <div key={product.id} className="card">
-            <h3>{product.name}</h3>
+      <div className="slider-container">
+        <button className="prev">Last</button>
+        <Slider {...settings}>
+            {products.map((product) => (
+              <div key={product.id} className="similarCard">
+                <h3>{product.name}</h3>
+              </div>
+            ))}
+        </Slider>
+        <button className="next">Next</button>
+      </div>
+      <div className="slider-container">
+        <button className="prev">Last</button>
+        <Slider {...settings2}>
+          <div className="outfitCard">
+            <button>Add to Outfit</button>
           </div>
-          </Slider>
-          <li key={product.id}>{product.name}</li>
-          </>
-        ))}
-      </ul>
+        </Slider>
+        <button className="next">Next</button>
+      </div>
     </>
   );
 };
