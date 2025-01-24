5f78d0fe0245736d88c21eccc64661ecef92bf5f
Kevin <kevinvar2805@gmail.com>
2025-01-24 00:53:26 -0600

    fixed typo

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import App from './components/App.jsx';
 import { createRoot } from 'react-dom/client';
 import {Provider} from 'react-redux';
-import STORE from './store/store.js';
+import STORE from './store/Store.js';
 const root = createRoot(document.getElementById('root'));
 root.render(
 <Provider store={STORE}>
