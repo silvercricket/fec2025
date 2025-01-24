a977e7e788d972b69788ac4066bbaa6094a88ff2
albertolovell <alberto.lovell@gmail.com>
2025-01-23 16:12:24 -0800

    fetch products function and useEffect

--- client/src/components/Similar/similar.jsx
+++ client/src/components/Similar/similar.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 
-const Similar = ({product, setProduct}) => (
-  <div>
-    Similar products go here!
 
-  </div>
-)
+const Similar = ({product, setProduct}) => {
+
+  const [similar, setSimilar] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = () => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/?page=1', {headers: {Authorization: process.env.AUTH_SECRET}})
+    .then((response) => {
+      setProducts(response.data);
+    })
+    .catch((err) => {
+      console.error('Similar failed', err);
+    })
+  }
+
+    React.useEffect(() => {
+      getProducts()
+    }, []);
+
+  return (
+    <>
+      <div>
+        Similar products go here!
+      </div>
+      <ul>
+        {products.map((product) => (
+          <li key={product.product_id}>{product.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 
 export default Similar;
