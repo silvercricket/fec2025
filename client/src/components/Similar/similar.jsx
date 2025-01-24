81a0bbaa057a05383cd9117b073f3f15fa3c881e
albertolovell <alberto.lovell@gmail.com>
2025-01-23 18:31:27 -0800

    implementing carousel

--- client/src/components/Similar/similar.jsx
+++ client/src/components/Similar/similar.jsx
@@ -23,6 +23,14 @@ const Similar = ({product, setProduct}) => {
       getProducts()
     }, []);
 
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1
+    };
+
   return (
     <>
       <div>
@@ -30,7 +38,14 @@ const Similar = ({product, setProduct}) => {
       </div>
       <ul>
         {products.map((product) => (
+          <>
+          <Slider {...settings}>
+          <div key={product.id} className="card">
+            <h3>{product.name}</h3>
+          </div>
+          </Slider>
           <li key={product.id}>{product.name}</li>
+          </>
         ))}
       </ul>
     </>
@@ -44,3 +59,17 @@ export default Similar;
 // https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40344
 // https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40344/related
 // https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40344/styles
+
+// product[0] = {
+//   campus: "hr-rfp"
+//   category: "Jackets"
+//   created_at: "2021-08-13T14:38:44.509Z"
+//   default_price: "140.00"
+//   description: "The So Fatigues will wake you up and fit you in.
+//                  This high energy camo will have you blending in to even the
+//                  wildest surroundings."
+//   id: 40344
+//   name: "Camo Onesie"
+//   slogan: "Blend in to your crowd"
+//   updated_at: "2021-08-13T14:38:44.509Z"
+// }
