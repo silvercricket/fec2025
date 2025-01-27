5f71af12c1753da061bee43f482a82bc288e1b3f
silvercricket <37115695+silvercricket@users.noreply.github.com>
2025-01-27 11:27:08 -0800

    ah

--- client/src/components/Overview/overview.jsx
+++ client/src/components/Overview/overview.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {ProductActions} from '../../store/ProductSlice.js';
-
-const Overview = () => (
-  <div data-testid="overview">
-import {OverviewActions} from '../../store/OverviewSlice.js';
 import MainDisplay from './overviewComponents/MainDisplay.jsx'
 import Share from './overviewComponents/Share.jsx'
 import Gallery from './overviewComponents/Gallery.jsx';
-import Checkout from './overviewComponents/Checkout.jsx'
+import Checkout from './overviewComponents/Checkout.jsx';
+import {OverviewActions} from '../../store/OverviewSlice.js';
 const Overview = () => {
-  const dispatch = useDispatch();
-  const Product = useSelector(store => store.Product);
-  const Overview = useSelector(store => store.Overview);
-  dispatch(OverviewActions.setOverview('AHHHHHHHHHHHHH'));
+  // const dispatch = useDispatch();
+  // const Product = useSelector(store => store.Product);
+  // const Overview = useSelector(store => store.Overview);
+  // dispatch(OverviewActions.setOverview('AHHHHHHHHHHHHH'));
   console.log(Product.product);
   console.log(Overview);
   return(
-  <div>
+  <div data-testid="overview">
     Overview goes here!
     <MainDisplay />
     <Gallery />
