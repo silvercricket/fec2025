d05fe9e40602748417f1b797fa8fd3291d71c139
Kevin <kevinvar2805@gmail.com>
2025-01-27 12:26:49 -0600

    Bug fixes for the recent merge

--- client/src/tests/App.test.jsx
+++ client/src/tests/App.test.jsx
@@ -1,34 +1,21 @@
 
 import React from 'react';
-import {fireEvent, render} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from '../components/App.jsx';
 import '@testing-library/jest-dom'
 import STORE from '../store/Store.js';
 import {Provider} from 'react-redux';
-import './matchMedia.mock'; // Must be imported before the tested file
-
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
 
 describe('App',()=>{
-  it('Should render', () => {
+
+  it('Should render app and the four main components', () => {
     const Apple = render(
       <Provider store={STORE}>
         <App/>
       </Provider>
     );
-    console.log(Apple);
+
+    expect(Apple.getByTestId('app')).toBeDefined();
 
     expect(Apple.getByTestId('overview')).toBeDefined();
 
@@ -38,4 +25,5 @@ describe('App',()=>{
 
     expect(Apple.getByTestId('review')).toBeDefined();
   });
+
 });
