8e8eae79729f94c628d823b133ef55edd4341cd3
albertolovell <alberto.lovell@gmail.com>
2025-01-29 11:43:21 -0800

    removed css imports

--- client/src/components/Similar/Compare.jsx
+++ client/src/components/Similar/Compare.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import '../../input.css';
 import PropTypes from 'prop-types';
-import '../../../dist/styles/index.css';
 
 const Compare = ({ currentProduct, starClicked, onClose }) => {
 
--- client/src/components/Similar/Outfit.jsx
+++ client/src/components/Similar/Outfit.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import '../../input.css';
 import PropTypes from 'prop-types';
-import '../../../dist/styles/index.css';
 
 const Outfit = ({ currentProduct }) => {
 
