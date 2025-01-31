4c5b609e52058debe19822dc52b83b5677208b8f
albertolovell <alberto.lovell@gmail.com>
2025-01-31 12:32:17 -0800

    modal close test

--- client/src/tests/Similar/Similar.test.jsx
+++ client/src/tests/Similar/Similar.test.jsx
@@ -9,6 +9,7 @@ import Similar from '../../components/Similar/similar.jsx';
 import Carousel from '../../components/Similar/Carousel.jsx';
 import Compare from '../../components/Similar/Compare.jsx';
 import Outfit from '../../components/Similar/Outfit.jsx';
+import Hover from '../../components/Similar/Hover.jsx'
 
 describe('Similar', () => {
 
@@ -22,6 +23,24 @@ describe('Similar', () => {
     expect(Related.getByTestId('similar')).toBeDefined();
 
   });
+
+  test('modal shows the children and a close button', () => {
+
+    const handleClose = jest.fn();
+
+    const {getByText} = render(
+      <Compare onClose={handleClose}>
+        <div>test</div>
+      </Compare>
+    )
+
+    expect(getByText('test')).toBeTruthy();
+
+    fireEvent.click(getByText(/close/i));
+
+    expect(handleClose).toHaveBeenCalledTimes(1);
+
+  })
 
   // it('Should render modal after ⭐ click', async () => {
   //   const handleOnClick = jest.fn();
