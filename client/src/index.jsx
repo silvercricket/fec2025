f9428a0a2ad6418a11ac09e64ba084b8db01cf58
Kevin <kevinvar2805@gmail.com>
2025-02-03 13:54:40 -0600

    Added some bug fixes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-// import './assets/base.css';
-// import './assets/styles.css';
 import App from './components/App.jsx';
 import { createRoot } from 'react-dom/client';
 import {Provider} from 'react-redux';
 import STORE from './store/Store.js';
 import '../dist/styles/index.css';
+import logo from './east_blue_logo.jpg';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
 <React.StrictMode>
 <Provider store={STORE}>
-  <App/>
+  <App logo={logo} />
 </Provider>
 </React.StrictMode>
 
