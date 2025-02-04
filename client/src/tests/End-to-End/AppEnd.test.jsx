0ed6ffd5d67ffd67faa5ba9c31f8d4b499536387
albertolovell <alberto.lovell@gmail.com>
2025-02-04 00:24:01 -0800

    args no-sandbox linux

--- client/src/tests/End-to-End/AppEnd.test.jsx
+++ client/src/tests/End-to-End/AppEnd.test.jsx
@@ -2,7 +2,11 @@ const puppeteer = require('puppeteer');
 
 describe('End-to-End Test', () => {
   test('should display the homepage', async () => {
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      timeout: 10000,
+    });
     const page = await browser.newPage();
     await page.goto('http://google.com');
     await expect(page.title()).resolves.toMatch('Google');
