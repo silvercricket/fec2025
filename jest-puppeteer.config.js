a4a89c7dcb8798b7e148d7c5bb353b84e9789382
albertolovell <alberto.lovell@gmail.com>
2025-02-04 11:34:19 -0800

    revert to jsdom default for main merge

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   launch: {
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: require('puppeteer').executablePath()
+    timeout: 60000,
   }
 }
