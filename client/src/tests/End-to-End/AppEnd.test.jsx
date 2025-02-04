205636726bc477e4ffbaa6a2ee80ad66a489527f
albertolovell <alberto.lovell@gmail.com>
2025-02-04 00:12:55 -0800

    headless: true

--- client/src/tests/End-to-End/AppEnd.test.jsx
+++ client/src/tests/End-to-End/AppEnd.test.jsx
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 describe('End-to-End Test', () => {
   test('should display the homepage', async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.goto('http://google.com');
     await expect(page.title()).resolves.toMatch('Google');
