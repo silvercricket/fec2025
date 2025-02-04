c889d344934d89db8a095ba44da66c93589b52ad
albertolovell <alberto.lovell@gmail.com>
2025-02-04 01:13:00 -0800

    revert test

--- client/src/tests/End-to-End/AppEnd.test.jsx
+++ client/src/tests/End-to-End/AppEnd.test.jsx
@@ -1,13 +1,19 @@
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 // const WebSocket = require('ws');
 
-describe('Google', () => {
-  beforeAll(async () => {
-    await page.goto('https://google.com');
-  });
-
-  it('should be titled "Google"', async () => {
+describe('End-to-End Test', () => {
+  test('should display the homepage', async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: puppeteer.executablePath(),
+      timeout: 10000,
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1000, height: 750 });
+    await page.goto('http://google.com');
     await expect(page.title()).resolves.toMatch('Google');
+    await browser.close();
   });
 });
 
