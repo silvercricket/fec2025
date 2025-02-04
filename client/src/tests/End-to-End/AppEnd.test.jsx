cd599edeaf47d8c73678013df4ef62e6d8d6f0df
albertolovell <alberto.lovell@gmail.com>
2025-02-04 02:10:13 -0800

    jest-puppeteer config

--- client/src/tests/End-to-End/AppEnd.test.jsx
+++ client/src/tests/End-to-End/AppEnd.test.jsx
@@ -1,10 +1,11 @@
 describe('Google', () => {
+
   beforeAll(async () => {
-    await global.page.goto('https://google.com');
+    await page.goto('https://google.com');
   });
 
   it('should be titled "Google"', async () => {
-    await expect(global.page.title()).resolves.toMatch('Google');
+    await expect(page.title()).resolves.toMatch('Google');
   });
 });
 
