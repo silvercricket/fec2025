a36feeb814466e69fa9fc27a868681b3f7829ad8
Kevin <kevinvar2805@gmail.com>
2025-02-06 17:05:43 -0600

    please work

--- client/src/components/Q&A/Q&AComponents/Photo.jsx
+++ client/src/components/Q&A/Q&AComponents/Photo.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Modal from '../../Reviews/PhotoModal.jsx';
 import PropTypes from 'prop-types';
-
+import {Image} from 'next/image';
 // eslint-disable-next-line react/prop-types, no-unused-vars
 const Photo = ({image}) => {
   const [open, setOpen] = React.useState(false);
@@ -11,9 +11,9 @@ const Photo = ({image}) => {
   const handleClose = () => setOpen(false);
   return (
     <>
-      <img data-testid="image" onClick={handleOpen} style={{height: '4em', width: '6em'}} src="https://i.ibb.co/1YscJG4P/Common-Coqui1-web-sized-1.webp" alt="Coqui" border="0"/>
+      <Image data-testid="image" onClick={handleOpen} style={{height: '4em', width: '6em'}} src="https://i.ibb.co/1YscJG4P/Common-Coqui1-web-sized-1.webp" alt="Coqui" border="0"/>
       <Modal isOpen={open} onClose={handleClose}>
-        <img data-testid="modal-image" style={{ maxWidth: '100%', maxHeight: 'auto', objectFit: 'contain'}} src="https://i.ibb.co/1YscJG4P/Common-Coqui1-web-sized-1.webp" alt="coqui"/>
+        <Image data-testid="modal-image" style={{ maxWidth: '100%', maxHeight: 'auto', objectFit: 'contain'}} src="https://i.ibb.co/1YscJG4P/Common-Coqui1-web-sized-1.webp" alt="coqui"/>
       </Modal>
     </>
   );
