3e6f7308295acba5ecdcdb058bb8efc614c75df1
Kevin <kevinvar2805@gmail.com>
2025-02-05 22:37:42 -0600

    public

--- app/[[...slug]]/client.js
+++ app/[[...slug]]/client.js
@@ -3,12 +3,14 @@ import React from 'react';
 import dynamic from 'next/dynamic'
 import {Provider} from 'react-redux';
 import STORE from '../../client/src/store/Store.js';
+import logo from '../../public/east_blue_logo.jpg';
+import '../../public/index.css'
 const App = dynamic(() => import('../../client/src/components/App'), { ssr: false })
 
 export function ClientOnly() {
   return (
     <Provider store={STORE}>
-      <App />
+      <App logo={logo}/>
     </Provider>
   )
 }
--- app/[[...slug]]/page.js
+++ app/[[...slug]]/page.js
@@ -1,10 +1,8 @@
 import { ClientOnly } from './client'
 import React from 'react';
-import {Provider} from 'react-redux';
-import STORE from '../../client/src/store/Store.js';
 
 const Page = () => {
-  return <ClientOnly />
+  return <ClientOnly/>
 }
 export function generateStaticParams() {
   return [{ slug: [''] }]
