193dd16f393e02697278c5a3bbfa9762676d97b5
Kevin <kevinvar2805@gmail.com>
2025-02-05 23:42:47 -0600

    Added icon

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const RootLayout = ({ children }) => {
     <html>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="icon" href="/icon" sizes="any" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500&display=swap" rel="stylesheet"/>
       </head>
